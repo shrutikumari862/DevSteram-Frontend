ac3f20010e278d9b96dfd98588040141bda7508b
shrutikumari862 <810kumarishruti@gmail.com>
2025-10-21 00:06:34 +0530

    commit

--- Devstream/src/socket.js
+++ Devstream/src/socket.js
@@ -5,7 +5,7 @@ export const initSocket=async()=>{
     const option={
         forceNew: true,
         reconnectionAttempts:Infinity,
-        timeout:1000,
+        timeout:10000,
         transports:['websocket']
     }
     return io(import.meta.env.VITE_BACKEND_URL, option)
